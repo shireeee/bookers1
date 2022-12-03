615a96a729de3314e589f1b13192bbc4d43a4bcb
EC2 Default User <115521591+shireeee@users.noreply.github.com>
2022-12-03 09:17:04 +0000

    [Finish] 2-13

---
+++ db/migrate/20221122044713_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
